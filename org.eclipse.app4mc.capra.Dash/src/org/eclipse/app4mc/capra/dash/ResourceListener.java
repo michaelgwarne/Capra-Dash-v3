17a1519a6ea85ea81578d4afbcedc93193b1b8ab
Michael Warne <michaelgwarne@gmail.com>
2016-05-04 14:24:07 +0200

    removed a test statement

--- org.eclipse.app4mc.capra.Dash/src/org/eclipse/app4mc/capra/dash/ResourceListener.java
+++ org.eclipse.app4mc.capra.Dash/src/org/eclipse/app4mc/capra/dash/ResourceListener.java
@@ -93,7 +93,7 @@ public class ResourceListener implements IResourceChangeListener{
 					IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
 					EList<ArtifactWrapper> list = awc.get().getArtifacts();
 					for (ArtifactWrapper aw : list) {
-						System.out.println(aw.getUri().toString() + " fuck " + delta.getResource().getFullPath().toString());
+						
 						if(aw.getUri().toString().equals(delta.getResource().getFullPath().toString())){
 
 							IMarker marker = file.createMarker("org.eclipse.app4mc.capra.Dash.mytracemarker");
